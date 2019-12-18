92ed7db4e150a820e27ef2c5440ffa0f145b3c38
TchenioGuillaume <44460505+TchenioGuillaume@users.noreply.github.com>
2019-12-18 17:12:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mario</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="reset.css">
+  </head>
+  <?php
+    include '../header.php';
+  ?>
+  <body>
+
+    <div class="fond">
+      <div class="mario"></div>
+      <div class="sol"></div>
+    </div>
+
+  </body>
+</html>
